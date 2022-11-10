6128125b81c3a5a3a873d60292cafdd5f4fbc24d
Bryan alay <bryanalmi27@hotmail.com>
2022-11-09 19:13:40 -0500

    automatic slide in header

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 
-const images = [
-    'img/img1.jpg',
-    'img/img2.jpg',
-    'img/img3.jpg',
-    'img/img4.jpg'
-]
+const images = ['img/img1.jpg','img/img2.jpg','img/img3.jpg','img/img4.jpg']
+const navesIMG = ['../img/bannerSpacex.png','../img/nave2.jpg','../img/nave3.jpg','../img/nave4.jpg']
 
 document.getElementById("screen").src = images[0];
 
 var sliderderecha = document.querySelector('.slider-der');
 var sliderizquierda = document.querySelector('.slider-iz');
 var contador = 0;
+var counter = 0;
+
+function bannerMove(){
+    counter++;
+    if(counter > navesIMG.length-1) counter = 0;
+    var a = document.getElementById('bannersecs')
+    a.style.backgroundImage = 'url('+navesIMG[counter]+')';
+}
+setInterval(bannerMove,2500);
 
 function derMove(){
     contador++;
@@ -27,5 +32,5 @@ function izMove(){
 }
 
 sliderderecha.addEventListener("click", derMove)
-
 sliderizquierda.addEventListener("click", izMove)
+
